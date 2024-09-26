c86c19b4507e5c0c6656abfc41f5b3131cc41b78
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-26 20:31:32 +0530

    create server socket

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,11 @@
-
-// Import required packages
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Main {
+public class Client {
     public static void main(String[] args) throws IOException {
-
-
         // Connect to the server on localhost at port 8080
         Socket socket = new Socket("localhost", 8080);
 
@@ -22,9 +18,10 @@ public class Main {
 
         String response = in.readLine();
 
-        System.out.println("Server response : " + response);
+        System.out.println("Client.Server response : " + response);
 
         // Close the connection
         socket.close();
     }
+
 }
---
+++ src/Server.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+
+        // Create a server socket listening on port 8080
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Server is listening on port 8080");
+
+
+        // Accept a client connection
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected!");
+        // Communicate with the client
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        // Read the client's message
+
+
+        String message = in.readLine();
+        System.out.println("Client says: " + message);
+
+
+        // Send a response to the client
+        out.println("Hello, Client!");
+
+
+        // Close the connection
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
